eff8cc68a24f65bcf4c328aca0a3d27a52dba002
aptmoo <socrates.de.filosoof.ll@gmail.com>
2022-09-05 15:44:25 +0200

    Laten we hopen dat dit werkt

---
+++ ch4/Opgave 2/3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAX_BUFFER_SIZE 64
+
+int is_schrikkel(int);
+
+int main()
+{
+    char buffer[MAX_BUFFER_SIZE];
+    int i;
+
+    fgets(buffer, MAX_BUFFER_SIZE, stdin);
+    i = atoi(buffer);
+
+    if(is_schrikkel(i))
+        printf("Jaar %d is een schrikkeljaar.\n", i);
+    else
+        printf("Jaar %d is geen schrikkeljaar.\n", i);
+}
+
+int is_schrikkel(int jaar)
+{
+    return ((jaar % 4 == 0 && jaar % 100 != 0) || jaar % 400 == 0);
+}
