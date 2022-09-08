13d5922fb5a9d910214e9138803785aef4601d18
aptmoo <socrates.de.filosoof.ll@gmail.com>
2022-09-08 14:25:55 +0200

    a

--- ch4/Opgave 3/2.c
+++ ch4/Opgave 3/2.c
@@ -2,7 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 
+int string_staartstuk(char[], char[]);
+
 int main()
 {
-    printf("%d\n", strlen("123456789"));
+    int i = string_staartstuk("abcdef", "defg");
+    printf("%d\n", i);
+}
+
+int string_staartstuk(char s[], char t[])
+{
+    int x, y;
+    x = strlen(s) - strlen(t);
+    printf("%d\n", x);
+    if(x < 0) return 0;
+
+    for(y = 0;s[x] != '\0' && t[y] != '\0' && t[y] == s[x]; x++, y++);
+
+    if(t[y] == '\0') return 1;
+    return 0;
 }
