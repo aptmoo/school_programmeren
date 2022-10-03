e383f8ac5fa0daebd4f92591895af116df628d29
aptmoo <socrates.de.filosoof.ll@gmail.com>
2022-10-03 15:15:11 +0200

    a

---
+++ ch7/1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int minlog(int, int);
+
+int main(int argc, char const *argv[])
+{
+    printf("ye %d\n", minlog(2, 4004));
+    printf("%f\n", logf(4004));
+    return 0;
+}
+
+int minlog(int g, int x)
+{
+    int m;
+    for(m = 1; (pow(g, m) <= x); m++)
+        ;
+    return m;
+}
